abab551b567618c624d31adcf31f666665651985
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-05-10 16:04:36 +0530

    Create 30 - Constructor Demo.cpp

---
+++ rajesh-samata/C++/30 - Constructor Demo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Class definition with a constructor
+class MyClass {
+public:
+    // Constructor
+    MyClass() {
+        cout << "Constructor called!" << endl;
+    }
+};
+
+int main() {
+    // Creating an object of MyClass
+    MyClass obj;
+
+    return 0;
+}
